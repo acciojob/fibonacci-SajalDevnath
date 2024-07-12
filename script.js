c0e718805b6ff85a67b1db2d937cdf3df2c5cc4a
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-12 17:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0, b = 1, c;
+    for (let i = 2; i <= num; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return a;
+  }
 }
-
-module.exports = fibonacci;
